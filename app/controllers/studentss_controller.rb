19137bbace78b93a07cfd38975bb189317d0110f
Josue Abraham <josuea@noukod.com>
2019-01-21 22:12:30 +0000

    Automatically backed up by Learn

--- app/controllers/studentss_controller.rb
+++ app/controllers/studentss_controller.rb
@@ -1,9 +1,27 @@
-class PostsController < ApplicationController
-	def index
-		@posts = Post.all
-	end
+class StudentsController < ApplicationController
+  before_action :set_student, only: :show
 
-	def show
-		@post = Post.find(params[:id])
-	end
+  def index
+    @students = Student.all
+  end
+
+  def show
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active
+      @student.active = false
+    else
+      @student.active = true
+    end
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
